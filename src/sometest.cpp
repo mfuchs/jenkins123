5b0df8995fb27b83447125a3aee7ee627e61896d
Matthias Fuchs <foo@example.com>
2016-08-17 20:45:08 +0200

    Fixes CMakeLists.txt for testing and add a failing test.

--- src/sometest.cpp
+++ src/sometest.cpp
@@ -2,8 +2,10 @@
 
 class SomeTest : public QObject
 {
+    Q_OBJECT
 private slots:
     void test_something();
+    void test_something2();
 };
 
 void SomeTest::test_something()
@@ -11,4 +13,10 @@ void SomeTest::test_something()
     QCOMPARE(10, 10);
 }
 
-QTEST_MAIN(SomeTest)
+void SomeTest::test_something2()
+{
+    QCOMPARE(10, 12);
+}
+
+QTEST_APPLESS_MAIN(SomeTest)
+#include "sometest.moc"
